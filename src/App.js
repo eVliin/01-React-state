8699f2198864aaaf9ceb57ca77ba3e9ed24ce0e1
eVliin <104331636+eVliin@users.noreply.github.com>
2022-07-18 17:19:18 -0300

    code rewrite

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import "./styles.css";
 import morango from "../public/assets/morango.png";
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Evelyn</h1>
-      <h2>{2021 - 2004}</h2>
-      <h3>massas</h3>
-      <ul>
-        <li>Mixed messages</li>
-        <li>5/4</li>
-        <li>online oversight</li>
-      </ul>
-      <img src={morango} />
-    </div>
-  );
+export default class aboutMe extends Component {
+  state = {
+    nome: "Evelyn",
+    idade: 2021 - 2004,
+    comida: "Massas",
+    msc: ["Mixed messages", "5/4", "online oversight"]
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.nome}</h1>
+        <h2>{this.state.idade}</h2>
+        <h3>{this.state.comida}</h3>
+        <ul>
+          <li>{this.state.msc[1]}</li>
+          <li>{this.state.msc[0]}</li>
+          <li>{this.state.msc[2]}</li>
+        </ul>
+        <img src={morango} />
+      </div>
+    );
+  }
 }
