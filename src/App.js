48939eb2b7f7329329c1b709ae84599c5449edab
eVliin <104331636+eVliin@users.noreply.github.com>
2022-08-10 19:03:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default class aboutMe extends Component {
           <li>{this.state.msc[0]}</li>
           <li>{this.state.msc[2]}</li>
         </ul>
-        <img src={morango} alt="morangos" />
+        <img src={morango} alt="ilustração de morangos cortados" />
       </div>
     );
   }
